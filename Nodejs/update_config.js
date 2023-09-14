ec5cc2033745842e622a2bfd05dbc3634853d035
EC2 Default User <ec2-user@ip-10-16-10-111.ec2.internal>
2023-09-14 07:22:56 +0000

    update connfig.js and confg.py

---
+++ Nodejs/update_config.js
@@ -0,0 +1,36 @@
+const AWS = require('aws-sdk');
+
+// Initialize the AWS SDK with your region
+AWS.config.update({ region: 'us-east-1' });
+
+// Create an AWS S3 client
+const s3 = new AWS.S3();
+
+async function uploadFile() {
+  try {
+    // Specify your bucket name
+    const bucketName = '<FMI_1>';
+
+    // Specify the local file path
+    const filename = '/home/ec2-user/environment/resources/website/config.js';
+
+    // Specify the S3 key (object key) for the file
+    const s3Key = 'config.js';
+
+    // Upload the file to S3 with the specified content type and cache control
+    await s3.upload({
+      Bucket: bucketName,
+      Key: s3Key,
+      Body: require('fs').createReadStream(filename),
+      ContentType: 'application/js',
+      CacheControl: 'max-age=0',
+    }).promise();
+
+    console.log('DONE');
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+// Call the uploadFile function to upload the file to S3
+uploadFile();

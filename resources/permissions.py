f8eff6e3e07eca42a41661abe76778be04bceba0
EC2 Default User <ec2-user@ip-10-16-10-111.ec2.internal>
2023-09-14 07:47:56 +0000

    change directory

--- resources/permissions.py
+++ resources/permissions.py
@@ -7,7 +7,7 @@ import json
 S3API = boto3.client("s3", region_name="us-east-1")
 bucket_name = "<FMI_1>"
 
-policy_file = open("/home/ec2-user/environment/resources/website_security_policy.json", "r")
+policy_file = open("/home/ec2-user/environment/pythonnodejs-lab6.1/resources/website_security_policy.json", "r")
 
 
 S3API.put_bucket_policy(
